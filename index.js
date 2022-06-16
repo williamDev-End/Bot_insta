bedbf8b58130c45ee5d630617122b68b2863576a
William Ng <william_ng94@hotmail.com>
2022-06-16 23:56:39 +0200

    init project, try connection

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+require('dotenv').config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: [
+      '--window-size=1920,1080',
+    ],
+  });
+  const page = await browser.newPage();
+  await page.setViewport({
+    height: 1080,
+    width: 1920,
+  });
+  await page.goto('https://instagram.com');
+  await page.click('body > div.RnEpo.Yx5HN._4Yzd2 > div > div > button.aOOlW.HoLwm');
+  await page.waitForSelector('input');
+  await page.type('[name=username]', process.env.INSTAGRAM_USERNAME);
+  await page.type('[name=password]', process.env.INSTAGRAM_PASSWORD);
+  await page.evaluate(() => {
+    document.querySelector('#loginForm > div > div:nth-child(3) > button').click();
+  });
+  await page.waitForSelector('input');
+  await page.evaluate(() => {
+    document.querySelector('.sqdOP').click();
+  });
+//   await browser.close();
+})();
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
